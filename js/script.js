07d4234a0d5e4cd9de091ebf66fccdf43724ce8c
casi-z <95171553+casi-z@users.noreply.github.com>
2022-01-26 18:12:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const link = document.querySelector('.link');
+
+let x = Math.random();
+console.log(x);
+if (x > 0.1) {
+    link.href = "https://vk.com/feed";
+    link.style.color = "green";
+}
+if (x > 0.15) {
+    link.href = "https://vk.com/id495886604";
+    link.style.color = "blue";
+}
+
+if (x > 0.2) {
+    link.href = "https://yandex.ru/";
+    link.style.color = "magenta";
+}
+if (x > 0.25) {
+    link.href = "https://school.mosreg.ru/feed";
+    link.style.color = "magenta";
+}
+if (x > 0.3) {
+    link.href = "https://www.google.ru/";
+    link.style.color = "violet"
+}
+if (x > 0.35) {
+    link.href = "https://www.instagram.com/";
+    link.style.color = "red";
+}
+if (x > 0.4) {
+    link.href = "https://ru-ru.facebook.com/";
+    link.style.color = "yellow";
+}
+if (x > 0.45) {
+    link.href = "https://mail.yandex.ru/#inbox";
+    link.style.color = "magenta";
+}
+if (x > 0.5) {
+    link.href = "https://mail.google.com/mail/u/0/#inbox";
+    link.style.color = "magenta";
+}
+if (x > 0.55) {
+    link.href = "https://www.kinopoisk.ru/";
+    link.style.color = "red";
+}
+if (x > 0.6) {
+    link.href = "https://vk.com/seryogataran2005";
+    link.style.color = "green";
+}
+if (x > 0.65) {
+    link.href = "https://vk.com/dayvinchik";
+    link.style.color = "violet"
+}
+if (x > 0.7) {
+    link.href = "https://twitter.com/";
+    link.style.color = "blue";
+}
+if (x > 0.75) {
+    link.href = "https://music.yandex.ru/home";
+    link.style.color = "blue";
+}
+if (x > 0.8) {
+    link.href = "https://open.spotify.com/";
+    link.style.color = "magenta";
+}
+if (x > 0.85) {
+    link.href = "https://ok.ru/";
+    link.style.color = "magenta";
+}
+if (x > 0.9) {
+    link.href = "https://rt.pornhub.com/";
+    link.style.color = "brown";
+}
+if (x > 0.95) {
+    link.href = "https://gdz.ru/";
+    link.style.color = "yellow";
+}
+
+
+
