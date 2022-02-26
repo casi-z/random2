0252c7723bfece2ceb9344cf2452c0bd03275ea4
casi-z <95171553+casi-z@users.noreply.github.com>
2022-02-26 17:19:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,76 +1,76 @@
 let x = Math.random();
 console.log(x);
 if (x > 0.1) {
-    window.location.href = "https://vk.com/feed";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366184";
     
 }
 if (x > 0.15) {
-    window.location.href = "https://vk.com/id495886604";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366187";
     
 }
 
 if (x > 0.2) {
-    window.location.href = "https://yandex.ru/";
+    window.location.href = "https://music.yandex.ru/album/3087311/track/34942409";
     
 }
 if (x > 0.25) {
-    window.location.href = "https://school.mosreg.ru/feed";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366182";
     
 }
 if (x > 0.3) {
-    window.location.href = "https://www.google.ru/";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366183";
     
 }
 if (x > 0.35) {
-    window.location.href = "https://www.instagram.com/";
+    window.location.href = "https://music.yandex.ru/album/2976568/track/28420997";
 
 }
 if (x > 0.4) {
-    window.location.href = "https://ru-ru.facebook.com/";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366185";
     
 }
 if (x > 0.45) {
-    window.location.href = "https://mail.yandex.ru/#inbox";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366198";
 
 }
 if (x > 0.5) {
-    window.location.href = "https://mail.google.com/mail/u/0/#inbox";
+    window.location.href = "https://music.yandex.ru/album/19103320/track/94324100";
 
 }
 if (x > 0.55) {
-    window.location.href = "https://www.kinopoisk.ru/";
+    window.location.href = "https://music.yandex.ru/album/19252173/track/94768097";
 
 }
 if (x > 0.6) {
-    window.location.href = "https://vk.com/taran16";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366194";
 
 }
 if (x > 0.65) {
-    window.location.href = "https://vk.com/dayvinchik";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366201";
 
 }
 if (x > 0.7) {
-    window.location.href = "https://twitter.com/";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366203";
 
 }
 if (x > 0.75) {
-    window.location.href = "https://music.yandex.ru/home";
+    window.location.href = "hhttps://music.yandex.ru/album/19739328/track/96366205";
 
 }
 if (x > 0.8) {
-    window.location.href = "https://open.spotify.com/";
+    window.location.href = "https://music.yandex.ru/album/19340407/track/95034407";
 
 }
 if (x > 0.85) {
-    window.location.href = "https://ok.ru/";
+    window.location.href = "https://music.yandex.ru/album/19739328/track/96366192";
 
 }
 if (x > 0.9) {
-    window.location.href = "https://rt.pornhub.com/";
+    window.location.href = "https://music.yandex.ru/album/3087311/track/28421145";
 
 }
 if (x > 0.95) {
-    window.location.href = "https://gdz.ru/";
+    window.location.href = "https://music.yandex.ru/album/3087311/track/28421150";
     
 }
 
