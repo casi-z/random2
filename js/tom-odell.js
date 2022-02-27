a9a71115bc87526b8d34e119015b6d79f56fad0a
casi-z <95171553+casi-z@users.noreply.github.com>
2022-02-27 12:51:29 +0300

    Add files via upload

---
+++ js/tom-odell.js
@@ -0,0 +1,407 @@
+let x = Math.random();
+console.log(x);
+if (x < 0.01) {
+	window.location.href = "https://vk.com/audio-2001884140_3884140";
+	
+}
+if (x > 0.01) {
+	window.location.href = "https://vk.com/audio-2001884140_3884140";
+	
+}
+if (x > 0.02) {
+    window.location.href = "https://vk.com/audio-2001884134_3884134";
+    
+}
+if (x > 0.03) {
+    window.location.href = "https://vk.com/audio-2001561954_91561954";
+    
+}
+
+if (x > 0.04) {
+    window.location.href = "https://vk.com/audio-2001884142_3884142";
+    
+}
+if (x > 0.05) {
+    window.location.href = "https://vk.com/audio-2001884131_3884131";
+    
+}
+if (x > 0.06) {
+    window.location.href = "https://vk.com/audio-2001799892_4799892";
+    
+}
+if (x > 0.07) {
+    window.location.href = "https://vk.com/audio-2001799894_4799894";
+
+}
+if (x > 0.08) {
+    window.location.href = "https://vk.com/audio-2001561957_91561957";
+    
+}
+if (x > 0.09) {
+    window.location.href = "https://vk.com/audio-2001561971_91561971";
+
+}
+if (x > 0.1) {
+    window.location.href = "https://vk.com/audio-2001561955_91561955";
+
+}
+if (x > 0.11) {
+    window.location.href = "https://vk.com/audio-2001561956_91561956";
+
+}
+if (x > 0.12) {
+    window.location.href = "https://vk.com/audio-2001799893_4799893";
+
+}
+if (x > 0.13) {
+    window.location.href = "https://vk.com/audio-2001561960_91561960";
+
+}
+if (x > 0.14) {
+    window.location.href = "https://vk.com/audio-2001884144_3884144";
+
+}
+if (x > 0.15) {
+    window.location.href = "https://vk.com/audio-2001446030_46446030";
+
+}
+if (x > 0.16) {
+    window.location.href = "https://vk.com/audio-2001884139_3884139";
+
+}
+if (x > 0.17) {
+    window.location.href = "https://vk.com/audio-2001884145_3884145";
+
+}
+if (x > 0.18) {
+    window.location.href = "https://vk.com/audio-2001884137_3884137";
+
+}
+if (x > 0.19) {
+    window.location.href = "https://vk.com/audio-2001884137_3884137";
+    
+}
+if (x > 0.2) {
+	window.location.href = "https://vk.com/audio-2001884141_3884141";
+	
+}
+if (x > 0.21) {
+	window.location.href = "https://vk.com/audio-2001884138_3884138";
+	
+}
+
+if (x > 0.22) {
+	window.location.href = "https://vk.com/audio-2001446043_46446043";
+	
+}
+if (x > 0.23) {
+	window.location.href = "https://vk.com/audio-2001799896_4799896";
+	
+}
+if (x > 0.24) {
+	window.location.href = "https://vk.com/audio-2001217787_8217787";
+	
+}
+if (x > 0.25) {
+	window.location.href = "https://vk.com/audio-2001446047_46446047";
+
+}
+if (x > 0.26) {
+	window.location.href = "https://vk.com/audio-2001561967_91561967";
+	
+}
+if (x > 0.27) {
+	window.location.href = "https://vk.com/audio-2001446050_46446050";
+
+}
+if (x > 0.28) {
+	window.location.href = "https://vk.com/audio-2001446035_46446035";
+
+}
+if (x > 0.29) {
+	window.location.href = "https://vk.com/audio-2001799901_4799901";
+
+}
+if (x > 0.3) {
+	window.location.href = "https://vk.com/audio-2001567553_7567553";
+
+}
+if (x > 0.31) {
+	window.location.href = "https://vk.com/audio-2001799898_4799898";
+
+}
+if (x > 0.32) {
+	window.location.href = "https://vk.com/audio-2001217785_8217785";
+
+}
+if (x > 0.33) {
+	window.location.href = "https://vk.com/audio-2001884135_3884135";
+
+}
+if (x > 0.34) {
+	window.location.href = "https://vk.com/audio-2001884135_3884135";
+
+}
+if (x > 0.35) {
+	window.location.href = "https://vk.com/audio-2001799900_4799900";
+
+}
+if (x > 0.36) {
+	window.location.href = "https://vk.com/audio-2001799902_4799902";
+
+}
+if (x > 0.37) {
+	window.location.href = "https://vk.com/audio-2001930112_3930112";
+	
+}
+if (x > 0.38) {
+	window.location.href = "https://vk.com/audio-2001561972_91561972";
+	
+}
+if (x > 0.39) {
+	window.location.href = "https://vk.com/audio-2001561978_91561978";
+	
+}
+
+if (x > 0.4) {
+	window.location.href = "https://vk.com/audio-2001561977_91561977";
+	
+}
+if (x > 0.41) {
+	window.location.href = "https://vk.com/audio-2001884136_3884136";
+	
+}
+if (x > 0.42) {
+	window.location.href = "https://vk.com/audio-2001561973_91561973";
+	
+}
+if (x > 0.43) {
+	window.location.href = "https://vk.com/audio-2001561974_91561974";
+
+}
+if (x > 0.44) {
+	window.location.href = "https://vk.com/audio-2001561976_91561976";
+	
+}
+if (x > 0.45) {
+	window.location.href = "https://vk.com/audio-2001930111_3930111";
+
+}
+if (x > 0.46) {
+	window.location.href = "https://vk.com/audio-2001333585_50333585";
+
+}
+if (x > 0.47) {
+	window.location.href = "https://vk.com/audio-2001446039_46446039";
+
+}
+if (x > 0.48) {
+	window.location.href = "https://vk.com/audio-2001799899_4799899";
+
+}
+if (x > 0.49) {
+	window.location.href = "https://vk.com/audio-2001446009_46446009";
+
+}
+if (x > 0.5) {
+	window.location.href = "https://vk.com/audio-2001799895_4799895";
+
+}
+// if (x > 0.51) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366205";
+
+// }
+// if (x > 0.52) {
+// 	window.location.href = "https://music.yandex.ru/album/19340407/track/95034407";
+
+// }
+// if (x > 0.53) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366192";
+
+// }
+// if (x > 0.54) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/28421145";
+
+// }
+// if (x > 0.55) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/28421150";
+	
+// }
+// if (x > 0.56) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366184";
+	
+// }
+// if (x > 0.57) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366187";
+	
+// }
+
+// if (x > 0.58) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/34942409";
+	
+// }
+// if (x > 0.59) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366182";
+	
+// }
+// if (x > 0.6) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366183";
+	
+// }
+// if (x > 0.61) {
+// 	window.location.href = "https://music.yandex.ru/album/2976568/track/28420997";
+
+// }
+// if (x > 0.62) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366185";
+	
+// }
+// if (x > 0.63) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366198";
+
+// }
+// if (x > 0.64) {
+// 	window.location.href = "https://music.yandex.ru/album/19103320/track/94324100";
+
+// }
+// if (x > 0.65) {
+// 	window.location.href = "https://music.yandex.ru/album/19252173/track/94768097";
+
+// }
+// if (x > 0.66) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366194";
+
+// }
+// if (x > 0.67) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366201";
+
+// }
+// if (x > 0.68) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366203";
+
+// }
+// if (x > 0.69) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366205";
+
+// }
+// if (x > 0.7) {
+// 	window.location.href = "https://music.yandex.ru/album/19340407/track/95034407";
+
+// }
+// if (x > 0.71) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366192";
+
+// }
+// if (x > 0.72) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/28421145";
+
+// }
+// if (x > 0.73) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/28421150";
+	
+// }
+// if (x > 0.74) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366184";
+	
+// }
+// if (x > 0.75) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366187";
+	
+// }
+
+// if (x > 0.76) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/34942409";
+	
+// }
+// if (x > 0.77) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366182";
+	
+// }
+// if (x > 0.78) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366183";
+	
+// }
+// if (x > 0.79) {
+// 	window.location.href = "https://music.yandex.ru/album/2976568/track/28420997";
+
+// }
+// if (x > 0.8) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366185";
+	
+// }
+// if (x > 0.81) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366198";
+
+// }
+// if (x > 0.82) {
+// 	window.location.href = "https://music.yandex.ru/album/19103320/track/94324100";
+
+// }
+// if (x > 0.83) {
+// 	window.location.href = "https://music.yandex.ru/album/19252173/track/94768097";
+
+// }
+// if (x > 0.84) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366194";
+
+// }
+// if (x > 0.85) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366201";
+
+// }
+// if (x > 0.86) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366203";
+
+// }
+// if (x > 0.87) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366205";
+
+// }
+// if (x > 0.88) {
+// 	window.location.href = "https://music.yandex.ru/album/19340407/track/95034407";
+
+// }
+// if (x > 0.89) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366192";
+
+// }
+// if (x > 0.9) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/28421145";
+
+// }
+// if (x > 0.91) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/28421150";
+	
+// }
+// if (x > 0.92) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366184";
+	
+// }
+// if (x > 0.93) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366187";
+	
+// }
+
+// if (x > 0.94) {
+// 	window.location.href = "https://music.yandex.ru/album/3087311/track/34942409";
+	
+// }
+// if (x > 0.95) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366182";
+	
+// }
+// if (x > 0.96) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366183";
+	
+// }
+// if (x > 0.97) {
+// 	window.location.href = "https://music.yandex.ru/album/2976568/track/28420997";
+
+// }
+// if (x > 0.98) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366185";
+	
+// }
+// if (x > 0.99) {
+// 	window.location.href = "https://music.yandex.ru/album/19739328/track/96366198";
+// }
